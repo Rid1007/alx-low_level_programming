7538379a6e4cf12bee4417e326d4b7582e02ed54
Rid1007 <ridwandamilola2020@gmail.com>
2022-09-11 06:43:50 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the numbers from 01 to 89
+ * Numbers must be seperated by , followed by a space
+ * Numbers should be printed in ascending order with 2 digits
+ * You can only use the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+int n1 = 48;
+int n2;
+int co■ = 44;
+while (n1 <= 57)
+{
+n2 = n1 + 1;
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(co■);
+putchar(32);
+}
+n2 += 1;
+}
+n1 += 1;
+}
+putchar('\n');
+return (0);
+}
